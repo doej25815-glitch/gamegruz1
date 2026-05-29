eb94fa540dda535d8967ceded17a305f6b6ace48
doej25815-glitch <doej25815@gmail.com>
2026-05-29 16:49:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
   description: "Clean Base mini app scaffold",
   other: {
     "base:app_id": "69e4837986272d70f28d742c",
+    "talentapp:project_verification" content="8c0c60650ba574b09bb9b4a820d6c6898c3d4626c3904fe77b30651395d0c04c63a4eb3737b1d4e96a187faec188b95cc1f8bc30037193d86f0fbfdb4d7497f2"
   },
 };
 
